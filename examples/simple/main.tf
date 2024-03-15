1dc2afe0c0570a978f803b5796e872caa3ff7f93
Shaked Braimok Yosef <shaked.braimok.yosef@gmail.com>
2024-03-15 08:50:02 +0200

    Update main.tf

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,7 +1,7 @@
 module "self_service_agent"{
     source = "Senora-dev/self-service-agent/aws"
     version = "~>1.0.0"
-    port_kafka_topics = ["org_qbNsLmREusEhrMk5.runs"]
-    port_kafka_bootstrap_servers = "b-3-public.publicclusterprod.t9rw6w.c1.kafka.eu-west-1.amazonaws.com:9196,b-1-public.publicclusterprod.t9rw6w.c1.kafka.eu-west-1.amazonaws.com:9196,b-2-public.publicclusterprod.t9rw6w.c1.kafka.eu-west-1.amazonaws.com:9196"
-    port_kafka_consumer_group_id = "Senora"
+    port_kafka_topics = ["PORT_ORG_ID.runs"]
+    port_kafka_bootstrap_servers = "kafka1:9096,kafka2:9096,kafka3:9096"
+    port_kafka_consumer_group_id = "PORT_KAFKA_CONSUMER_GROUP_ID"
 }
